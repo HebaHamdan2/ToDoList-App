3bac9eeaca4b3ec9194841d4db1944999bd8961c
HebaHamdan2 <hebasaeedhamdan296@gmail.com>
2024-10-19 18:02:35 +0300

    create model and send data to mongoDB

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,5 +1,19 @@
 import {  NextResponse } from "next/server";
-
+import TodoModel from "@/lib/models/TodoModel"
+import {ConnectDB} from "@/lib/config/db"
+const LoadDB=async()=>{
+    await ConnectDB();
+}
+LoadDB();
 export async function GET(){
 return NextResponse.json({msg:"get method hit"})
 }
+export async function POST(request){
+    const {title ,description}=await request.json();
+    await TodoModel.create({
+        title,
+        description
+    })
+    return NextResponse.json({msg:"Todo Created"})
+    }
+    
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 import Todo from "@/Components/Todo";
+import axios from "axios";
 import { useState } from "react";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -18,7 +19,8 @@ console.log(formData)
   const onSubmitHandler=async(e)=>{
     e.preventDefault();
     try{
-toast.success('Success');
+  const response=await axios.post('/api',formData)
+toast.success(response.data.msg);
     }catch(error){
  toast.error("Error")
     }
